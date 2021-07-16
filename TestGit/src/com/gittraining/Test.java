fbc24476fe9b86699238c6b5fa78287717b31e31
shrutiudyawar <shrutiudyawar@gmail.com>
2021-07-16 18:26:13 +0530

    minor changes to test.java file

--- TestGit/src/com/gittraining/Test.java
+++ TestGit/src/com/gittraining/Test.java
@@ -3,5 +3,6 @@ package com.gittraining;
 public class Test {
 	public static void main(String args[]) {
 		System.out.println("Hello world in java!");
+		System.out.println("This is test.java file!");
 	}
 }
